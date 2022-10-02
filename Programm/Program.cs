e91aa1a4e82ee9920a031d8628e42603692e899b
Danil Savrasov <Danukan74@gmail.com>
2022-10-02 19:42:54 +0500

    Programm - done

---
+++ Programm/Program.cs
@@ -0,0 +1,25 @@
+﻿string [] arrayFirst = new string[5] {"Denmark","Hello","12","345","10"};
+string [] arraySecond = new string [arrayFirst.Length];
+
+void ArrayConverter(string[] arr1,string[] arr2)
+{   
+    int  j=0;
+    for(int i =0 ;i<arr1.Length;i++)
+    {
+        if (arr1[i].Length<=3)
+        {
+            arr2[j]=arr1[i];
+            j++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayConverter(arrayFirst, arraySecond);
+PrintArray(arraySecond);
